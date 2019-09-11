7a908969a058ce1db1469b8d783c539b597a36ad
timer <timer_sevenwang@163.com>
2019-09-11 18:40:34 +0800

    update

--- Example/Pods/Target Support Files/Pods-SWAlertViewController_Tests/Pods-SWAlertViewController_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SWAlertViewController_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_SWAlertViewController_Tests
-@end
--- Example/SWAlertViewController/ViewController.swift
+++ Example/SWAlertViewController/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import SWAlertViewController
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let cacel = CFAlertAction.init(title: "cancel", style: .Default, alignment: .justified, backgroundColor: UIColor.white, textColor: UIColor.blue, textFont: UIFont.systemFont(ofSize: 12), cornerRadius: 15, borderColor: UIColor.blue, borderWidth: 1.0, handler: nil)
+        let aa = CFAlertAction.init(title: "comfir", style: .Default, alignment: .justified, backgroundColor: UIColor.blue, textColor: UIColor.white, textFont: UIFont.systemFont(ofSize: 12), cornerRadius: 15, borderColor: nil, borderWidth: nil, handler: nil)
+        let alert = CFAlertViewController.init(title: "test", message: "this is message", titleFont: UIFont.systemFont(ofSize: 17), messageFont: UIFont.systemFont(ofSize: 15), titleAndMessageSpace: 18, textAlignment: .center, margin: 18, cornerRadius: 8, borderColor: nil, borderWidth: nil, preferredStyle: .alert, actionsArrangement: .horizontal, didDismissAlertHandler: nil)
         
+        alert.addAction(cacel)
+        alert.addAction(aa)
+        present(alert, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
